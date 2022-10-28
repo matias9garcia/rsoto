a488b8364a39924d16eac69fc249d91d10c89971
matias9garcia <matiasgarciasaldes@gmail.com>
2022-10-27 22:02:23 -0300

    re-organize content for mii+
    create showcourse.js to show individual course
    
    create class for buttons
    
    hide all content by default

---
+++ assets/js/showcourse.js
@@ -0,0 +1,69 @@
+const [html] = document.getElementsByTagName("html")
+const lang = html.getAttribute("lang");
+
+  function myFunction(id) {
+
+    var btnText = document.getElementById(id);
+    var moreText;
+    var hideRest;
+
+    // course page
+    if(id == "showBtnMII-748"){
+        moreText = document.getElementById("span-mii-748");
+
+        // hide other courses REWORK
+        hideRest = document.getElementById("span-mii-771");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-774");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-779");
+        hideRest.style.display = "none";
+
+    } else if( id == "showBtnMII-771"){
+        moreText = document.getElementById("span-mii-771");
+
+        // hide other courses REWORK
+        hideRest = document.getElementById("span-mii-748");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-774");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-779");
+        hideRest.style.display = "none";
+
+    } else if( id == "showBtnMII-774"){
+        moreText = document.getElementById("span-mii-774");
+
+        // hide other courses REWORK
+        hideRest = document.getElementById("span-mii-748");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-771");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-779");
+        hideRest.style.display = "none";
+
+    } else if ( id == "showBtnMII-779"){
+        moreText = document.getElementById("span-mii-779");
+
+        // hide other courses REWORK
+        hideRest = document.getElementById("span-mii-748");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-771");
+        hideRest.style.display = "none";
+
+        hideRest = document.getElementById("span-mii-774");
+        hideRest.style.display = "none";
+    }
+
+
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline";
+      
+    }
+  }
